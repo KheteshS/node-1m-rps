6e4825f035bac6622774d97bfb1692a66908927c
KheteshS <kheteshs@gmail.com>
2026-09-14 16:22:55 +0530

    updated my local machine cores

--- playground/multi-thread.js
+++ playground/multi-thread.js
@@ -3,7 +3,11 @@ import { fileURLToPath } from "url";
 
 if (isMainThread) {
   // Spawn 12 system level threads
-  for (let i = 0; i < 12; i++) {
+  /* 
+    PowerShell: Execute Get-WmiObject –class Win32_processor | ft NumberOfCores,NumberOfLogicalProcessors to get the number of cores of your machine 
+    mine is 4
+  */
+  for (let i = 0; i < 4; i++) {
     new Worker(fileURLToPath(import.meta.url));
   }
 } else {
